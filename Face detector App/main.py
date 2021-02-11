8d66c519027d456fbc847ef6e579a28a3de54de9
daniel-bicu <danielyoi1233@gmail.com>
2021-02-11 14:22:50 +0200

    Initial commit.

---
+++ Face detector App/main.py
@@ -0,0 +1,7 @@
+# OpenCv - face detector app
+# HAARCASCADE algorithm = trained on tons of faces (front faces)
+# you can download dataset from opencv/data/haarcascades on Github
+import cv2
+
+if __name__ == '__main__':
+    print("Hey, AI!")
